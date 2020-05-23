71b546aac1fdc976ef058e62ccbb46e9f2425c59
Chen Wu <chen.wu@icrar.org>
2020-05-23 23:32:55 +0800

    get random forest local train working

--- examples/03/train/local_train.sh
+++ examples/03/train/local_train.sh
@@ -1,8 +1,8 @@
 
-python script.py --n-estimators 100 \
+python train.py --n-estimators 100 \
                    --min-samples-leaf 2 \
                    --model-dir ./ \
-                   --train ./ \
-                   --test ./ \
+                   --train ../../.. \
+                   --test ../../.. \
                    --features 'CRIM ZN INDUS CHAS NOX RM AGE DIS RAD TAX PTRATIO B LSTAT' \
                    --target target
