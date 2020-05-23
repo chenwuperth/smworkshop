a4620433e5d0ff6f294ebff72c9e819d1f55a371
Chen Wu <chen.wu@icrar.org>
2020-05-23 20:34:33 +0800

    endpoint invocation working

---
+++ examples/03/inference/invoke_endpoint.py
@@ -0,0 +1,20 @@
+"""
+Invoke deployed endpoint
+"""
+import os
+import boto3
+
+runtime = boto3.client('sagemaker-runtime')
+source_dir = os.path.abspath(os.path.dirname(__file__))
+csv_fp = os.path.join(source_dir, '..', 'train', 'boston_test.csv')
+endpoint_name = 'sagemaker-scikit-learn-2020-05-23-12-17-39-214'
+
+with open(csv_fp, 'r') as fin:
+    csv_body = fin.read()
+
+response = runtime.invoke_endpoint(
+    EndpointName=endpoint_name,
+    Body=csv_body.encode('utf-8'),
+    ContentType='text/csv')
+
+print(response['Body'].read())
